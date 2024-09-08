bd83c223055167ce433d5a3153649447a4919607
Sarthak Madaan <sarthakmadaan5@gmail.com>
2024-09-08 21:59:43 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (28.44%) - LeetSync

--- Leetcode/70-climbing-stairs/climbing-stairs.java
+++ Leetcode/70-climbing-stairs/climbing-stairs.java
@@ -1,10 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp=new int[n+1];
-        for(int i=0;i<dp.length;i++){
-            dp[i]=-1;
+        if(n==0)return 0;
+        if(n==1)return 1;
+        int prev0=1;
+        int prev1=1;
+        for(int i=2;i<=n;i++){
+            int temp=prev0+prev1;
+            prev0=prev1;
+            prev1=temp;
         }
-        return climbStairs(dp, n);
+        return prev1;
+
     }
     // recurisve solution
     //     if(n==0)return 1;
@@ -12,11 +18,16 @@ class Solution {
     //     return climbStairs(n-1)+climbStairs(n-2);
 
     // memoization
-    public int climbStairs(int[] dp, int n){
-        if(n==0)return 1;
-        if(n<0)return 0;
-        if(dp[n]!=-1)return dp[n];
-        return dp[n]=climbStairs(dp, n-1)+climbStairs(dp, n-2);
-    }
+    // int[] dp=new int[n+1];
+    //     for(int i=0;i<dp.length;i++){
+    //         dp[i]=-1;
+    //     }
+    //     return climbStairs(dp, n);
+    // public int climbStairs(int[] dp, int n){
+    //     if(n==0)return 1;
+    //     if(n<0)return 0;
+    //     if(dp[n]!=-1)return dp[n];
+    //     return dp[n]=climbStairs(dp, n-1)+climbStairs(dp, n-2);
+    // }
 
 }
