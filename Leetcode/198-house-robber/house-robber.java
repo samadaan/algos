fcbf626843a62e163202371aecbd1dd88d30536c
Sarthak Madaan <sarthakmadaan5@gmail.com>
2025-12-10 12:10:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (17.23%) - LeetSync

--- Leetcode/198-house-robber/house-robber.java
+++ Leetcode/198-house-robber/house-robber.java
@@ -1,29 +1,16 @@
 class Solution {
+    static int[] dp;
     public int rob(int[] nums) {
-        int[][] dp=new int[nums.length][2];
-        for(int i=0;i<nums.length;i++){
-            dp[i][0]=-1;
-            dp[i][1]=-1;
-
-        }
-        return memoization(dp, 0, nums, 1);
+        dp=new int[nums.length];
+        Arrays.fill(dp,-1);
+        return recurse(nums, 0);
     }
-    // recursve
-    // public int recurse(int currIndex, int[] nums, boolean allowed){
-    //     if(nums.length==currIndex)return 0;
-    //     if(allowed){
-    //         return Math.max(recurse(currIndex+1, nums, true), nums[currIndex]+recurse(currIndex+1, nums, false));
-    //     }
-    //     return recurse(currIndex+1, nums, true);
-    // }
-
-    public int memoization(int[][] dp, int currIndex, int[] nums, int allowed){
-        if(nums.length==currIndex)return 0;
-        if(dp[currIndex][allowed]!=-1)return dp[currIndex][allowed];
-        if(allowed==1){
-            return dp[currIndex][1]=Math.max(memoization(dp, currIndex+1, nums, 1), nums[currIndex]+memoization(dp, currIndex+1, nums, 0));
-        }
-        return dp[currIndex][0]=memoization(dp, currIndex+1, nums, 1);
-
+    public int recurse(int[] nums, int currIndex){
+        if(currIndex>=nums.length)return 0;
+        if(dp[currIndex]!=-1)return dp[currIndex];
+        int taken=nums[currIndex]+recurse(nums,currIndex+2);
+        int notTaken=recurse(nums,currIndex+1);
+        dp[currIndex] = Math.max(taken, notTaken);
+        return dp[currIndex];
     }
 }
