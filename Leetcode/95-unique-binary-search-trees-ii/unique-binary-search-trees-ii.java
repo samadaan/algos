06ebc17db0d0c0039bcd2b53417c2ee2461f1fe1
Sarthak Madaan <sarthakmadaan5@gmail.com>
2025-12-19 23:51:21 +0530

    Time: 1 ms (99.05%) | Memory: 46.6 MB (15.01%) - LeetSync

---
+++ Leetcode/95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        return genTrees(1, n);
+    }
+    List<TreeNode> genTrees(int left, int right){
+        List<TreeNode> ans=new ArrayList<>();
+        if(left>right){
+            ans.add(null);
+            return ans;
+        }
+        if(left==right){
+            ans.add(new TreeNode(left));
+            return ans;
+        }
+        for(int curr=left;curr<=right;curr++){
+            List<TreeNode> leftNodes=genTrees(left, curr-1);
+            List<TreeNode> rightNodes=genTrees(curr+1, right);
+            for(TreeNode leftNode: leftNodes){
+                for(TreeNode rightNode: rightNodes){
+                    TreeNode currNode=new TreeNode(curr);
+                    currNode.left=leftNode;
+                    currNode.right=rightNode;
+                    ans.add(clone(currNode));
+                }
+            }
+        }
+        return ans;
+    }
+    TreeNode clone(TreeNode curr){
+        TreeNode cloned=new TreeNode(curr.val);
+        cloned.left=curr.left;
+        cloned.right=curr.right;
+        return cloned;
+    }
+}
