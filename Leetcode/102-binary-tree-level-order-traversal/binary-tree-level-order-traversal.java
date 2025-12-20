7e3beb9f1e0a957b5c50eea841f89eb329c94f8f
Sarthak Madaan <sarthakmadaan5@gmail.com>
2025-12-20 15:36:52 +0530

    Time: 2 ms (20.52%) | Memory: 46.7 MB (44.03%) - LeetSync

--- Leetcode/102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ Leetcode/102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -17,17 +17,35 @@ class Solution {
     List<List<Integer>> ans;
     public List<List<Integer>> levelOrder(TreeNode root) {
         ans=new ArrayList<>();
-        traverse(root, 0);
+        // traverse(root, 0);
+        if(root==null){
+            return ans;
+        }
+
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            List<Integer> a=new ArrayList<>();
+            while(size-->0){
+                TreeNode rem=queue.remove();
+                a.add(rem.val);
+                if(rem.left!=null)queue.add(rem.left);
+                if(rem.right!=null)queue.add(rem.right);
+            }
+            ans.add(a);
+        }
         return ans;
         
     }
-    void traverse(TreeNode curr, int depth){
-        if(curr==null)return;
-        if(depth==ans.size()){
-            ans.add(new ArrayList<>());
-        }
-        traverse(curr.left, depth+1);
-        ans.get(depth).add(curr.val);
-        traverse(curr.right, depth+1);
-    }
+    // void traverse(TreeNode curr, int depth){
+    //     if(curr==null)return;
+    //     if(depth==ans.size()){
+    //         ans.add(new ArrayList<>());
+    //     }
+    //     traverse(curr.left, depth+1);
+    //     ans.get(depth).add(curr.val);
+    //     traverse(curr.right, depth+1);
+    // }
+
 }
