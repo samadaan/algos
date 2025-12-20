a765fab7f563612c8605316ec09128230e8edd7d
Sarthak Madaan <sarthakmadaan5@gmail.com>
2025-12-21 01:21:31 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (32.90%) - LeetSync

--- Leetcode/337-house-robber-iii/house-robber-iii.java
+++ Leetcode/337-house-robber-iii/house-robber-iii.java
@@ -14,30 +14,45 @@
  * }
  */
 class Solution {
-    Map<TreeNode, Integer> dpChosen;
-    Map<TreeNode, Integer> dpNotChosen;
+    // Map<TreeNode, Integer> dpChosen;
+    // Map<TreeNode, Integer> dpNotChosen;
 
     public int rob(TreeNode root) {
-        dpChosen = new HashMap<>();
-        dpNotChosen = new HashMap<>();
-        return Math.max(traverse(root, true), traverse(root, false));
+        // dpChosen = new HashMap<>();
+        // dpNotChosen = new HashMap<>();
+        // return Math.max(traverse(root, true), traverse(root, false));
+        int[] s=sum(root);
+        return Math.max(s[0], s[1]);
     }
 
-    int traverse(TreeNode curr, boolean canChoose){
-        if(curr==null)return 0;
-        if(canChoose && dpChosen.get(curr)!=null){
-            return dpChosen.get(curr);
-        }else if(dpNotChosen.get(curr)!=null){
-            return dpNotChosen.get(curr);
-        }
-        int sum1 = traverse(curr.left, true)+traverse(curr.right, true);
-        if(canChoose){
-            int sum2 = curr.val + traverse(curr.left, false)+traverse(curr.right, false);
-            dpChosen.put(curr, Math.max(sum1, sum2));
-            return dpChosen.get(curr);
-        }
-         dpNotChosen.put(curr, sum1);
-        return dpNotChosen.get(curr);
+    // int traverse(TreeNode curr, boolean canChoose){
+    //     if(curr==null)return 0;
+    //     if(canChoose && dpChosen.get(curr)!=null){
+    //         return dpChosen.get(curr);
+    //     }else if(dpNotChosen.get(curr)!=null){
+    //         return dpNotChosen.get(curr);
+    //     }
+    //     int sum1 = traverse(curr.left, true)+traverse(curr.right, true);
+    //     if(canChoose){
+    //         int sum2 = curr.val + traverse(curr.left, false)+traverse(curr.right, false);
+    //         dpChosen.put(curr, Math.max(sum1, sum2));
+    //         return dpChosen.get(curr);
+    //     }
+    //      dpNotChosen.put(curr, sum1);
+    //     return dpNotChosen.get(curr);
         
+    // }
+
+    int[] sum(TreeNode curr){
+        if(curr==null)return new int[]{0,0};
+
+        int[] left=sum(curr.left);
+
+        int[] right=sum(curr.right);
+
+        int sum1=curr.val+left[1]+right[1];
+        int sum2=left[0]+right[0];
+
+        return new int[]{Math.max(sum1, sum2), sum2};
     }
 }
