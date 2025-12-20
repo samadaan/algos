ec491e9aef1d25b55b6753d982619900d21bc894
Sarthak Madaan <sarthakmadaan5@gmail.com>
2025-12-21 01:10:32 +0530

    Time: 3 ms (13.61%) | Memory: 47.2 MB (6.21%) - LeetSync

---
+++ Leetcode/337-house-robber-iii/house-robber-iii.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Map<TreeNode, Integer> dpChosen;
+    Map<TreeNode, Integer> dpNotChosen;
+
+    public int rob(TreeNode root) {
+        dpChosen = new HashMap<>();
+        dpNotChosen = new HashMap<>();
+        return Math.max(traverse(root, true), traverse(root, false));
+    }
+
+    int traverse(TreeNode curr, boolean canChoose){
+        if(curr==null)return 0;
+        if(canChoose && dpChosen.get(curr)!=null){
+            return dpChosen.get(curr);
+        }else if(dpNotChosen.get(curr)!=null){
+            return dpNotChosen.get(curr);
+        }
+        int sum1 = traverse(curr.left, true)+traverse(curr.right, true);
+        if(canChoose){
+            int sum2 = curr.val + traverse(curr.left, false)+traverse(curr.right, false);
+            dpChosen.put(curr, Math.max(sum1, sum2));
+            return dpChosen.get(curr);
+        }
+         dpNotChosen.put(curr, sum1);
+        return dpNotChosen.get(curr);
+        
+    }
+}
