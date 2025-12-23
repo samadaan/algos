4910a101220a0ec2b152eebbcc7e20600e8416b5
Sarthak Madaan <sarthakmadaan5@gmail.com>
2025-12-23 23:43:01 +0530

    Time: 1 ms (62.40%) | Memory: 45.7 MB (5.62%) - LeetSync

---
+++ Leetcode/925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int curr;
+    Map<Integer, Integer> postOrderIndexMap;
+    int[] pre;
+
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+        postOrderIndexMap = new HashMap<>();
+        for (int i = 0; i < postorder.length; i++) {
+            postOrderIndexMap.put(postorder[i], i);
+        }
+        pre = preorder;
+        curr = 0;
+        return construct(0, pre.length - 1);
+    }
+
+    public TreeNode construct(int leftIndex, int rightIndex) {
+        if (leftIndex > rightIndex)
+            return null;
+        if (leftIndex == rightIndex)
+            return new TreeNode(pre[curr++]);
+        int val = pre[curr++];
+        int ind = postOrderIndexMap.get(val);
+        TreeNode n = new TreeNode(val);
+        int ind1 = postOrderIndexMap.get(pre[curr]);
+        n.left = construct(leftIndex, ind1);
+        n.right = construct(ind1 + 1, ind - 1);
+        return n;
+
+    }
+}
