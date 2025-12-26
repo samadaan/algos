781a4bb08b1b012e08e9372b82987e2e4a7128a9
Sarthak Madaan <sarthakmadaan5@gmail.com>
2025-12-26 15:28:15 +0530

    Time: 2 ms (98.21%) | Memory: 47.1 MB (13.57%) - LeetSync

--- Leetcode/1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.java
+++ Leetcode/1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.java
@@ -14,66 +14,37 @@
  * }
  */
 class Solution {
-    List<Integer> ans;
-    List<Integer> path;
+    String smallest = "";
 
     public String smallestFromLeaf(TreeNode root) {
-        ans = new ArrayList<>();
-        path = new ArrayList<>();
-        traverse(root);
-        String ansString = "";
-        for (int i = ans.size() - 1; i >= 0; i--) {
-            char x = (char) ('a' + ans.get(i));
-            System.out.print(x);
-            ansString += x;
-        }
-        return ansString;
+        dfs(root, new StringBuilder());
+        return smallest;
     }
 
-    private void traverse(TreeNode curr) {
-        if (curr == null)
-            return;
-        path.add(curr.val);
-        traverse(curr.left);
-        traverse(curr.right);
-        if (curr.left == null && curr.right == null) {
-            check();
-        }
-        path.remove(path.size() - 1);
-    }
+    private void dfs(TreeNode node, StringBuilder sb) {
+        if (node == null) return;
 
-    private void check() {
-        if (path.size() == 0) {
-            return;
-        }
-        if (ans.size() == 0) {
-            ans = clone(path);
-            return;
-        }
-        int a = ans.size() - 1;
-        int p = path.size() - 1;
-        while (p >= 0 && a >= 0) {
-            // System.out.print("p " + p + " a " + a + "\n");
-            if (path.get(p) < ans.get(a)) {
-                ans = clone(path);
-                return;
-            } else if (path.get(p) > ans.get(a)) {
-                return;
+        // Convert 0-25 to 'a'-'z' and append
+        sb.append((char)('a' + node.val));
+
+        // If it's a leaf, build the string (Leaf to Root) and compare
+        if (node.left == null && node.right == null) {
+            // We need to reverse because the path is built Root -> Leaf
+            String current = sb.reverse().toString();
+            
+            // Compare with the smallest found so far
+            if (smallest.equals("") || current.compareTo(smallest) < 0) {
+                smallest = current;
             }
-            p--;
-            a--;
-        }
-        if (a >= 0) {
-            ans = clone(path);
+            
+            // Reverse back so we don't mess up the backtracking for other branches
+            sb.reverse();
         }
 
-    }
+        dfs(node.left, sb);
+        dfs(node.right, sb);
 
-    private List<Integer> clone(List<Integer> inp) {
-        List<Integer> out = new ArrayList<>();
-        for (Integer i : inp) {
-            out.add(i);
-        }
-        return out;
+        // Backtrack: remove the current node's character
+        sb.setLength(sb.length() - 1);
     }
 }
