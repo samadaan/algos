a799be369032e211c4f15511d12c0c9f4de41666
Sarthak Madaan <sarthakmadaan5@gmail.com>
2025-12-26 19:12:39 +0530

    Time: 12 ms (2.65%) | Memory: 46.5 MB (6.62%) - LeetSync

---
+++ Leetcode/1194-path-in-zigzag-labelled-binary-tree/path-in-zigzag-labelled-binary-tree.java
@@ -0,0 +1,45 @@
+class Solution {
+    public List<Integer> pathInZigZagTree(int label) {
+        List<Integer> ans = new ArrayList<>();
+        while (label != 0) {
+            ans.add(0, label);
+            label = label / 2;
+        }
+
+        correct(ans);
+        return ans;
+
+    }
+
+    private void correct(List<Integer> ans) {
+        if (ans.size() % 2 == 0) {
+            for (int i = 1; i < ans.size() - 1; i++) {
+                if (i % 2 == 0) {
+                    int h = i + 1;
+                    int[] bounds = getBounds(h);
+                    int diff = ans.get(i) - bounds[0];
+                    int val = bounds[1] - diff;
+                    ans.set(i, val);
+                }
+            }
+        } else {
+            for (int i = 1; i < ans.size() - 1; i++) {
+                if (i % 2 == 1) {
+                    int h = i + 1;
+                    int[] bounds = getBounds(h);
+                    int diff = ans.get(i) - bounds[0];
+                    int val = bounds[1] - diff;
+                    ans.set(i, val);
+                }
+            }
+
+        }
+    }
+
+    private int[] getBounds(int x) {
+        if (x == 2) {
+            return new int[] { 2, 3 };
+        }
+        return new int[] { 2 * getBounds(x - 1)[0], 2 * getBounds(x - 1)[1] + 1 };
+    }
+}
