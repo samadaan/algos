3916bcd0d582989703691d1731881f4a477427f3
Sarthak Madaan <sarthakmadaan5@gmail.com>
2025-12-27 13:11:43 +0530

    Time: 3 ms (72.50%) | Memory: 53.7 MB (53.96%) - LeetSync

--- Leetcode/1039-find-the-town-judge/find-the-town-judge.java
+++ Leetcode/1039-find-the-town-judge/find-the-town-judge.java
@@ -1,20 +1,16 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
         int[] degreeArr = new int[n + 1];
-        Arrays.fill(degreeArr, 0);
-        int ans = -1;
         for (int[] x : trust) {
             degreeArr[x[0]]--;
             degreeArr[x[1]]++;
         }
         for (int i = 1; i <= n; i++) {
-            if (ans == -1 && degreeArr[i] == n - 1) {
-                ans=i;
-            } else if (degreeArr[i] == n - 1) {
-                return -1;
+            if (degreeArr[i] == n - 1) {
+                return i;
             }
         }
-        return ans;
+        return -1;
 
     }
 }
