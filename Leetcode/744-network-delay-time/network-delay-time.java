fad938128827a8e2ebe26728fd47494ea6908037
Sarthak Madaan <sarthakmadaan5@gmail.com>
2025-12-28 23:50:19 +0530

    Time: 9 ms (94.92%) | Memory: 49.4 MB (41.33%) - LeetSync

--- Leetcode/744-network-delay-time/network-delay-time.java
+++ Leetcode/744-network-delay-time/network-delay-time.java
@@ -22,20 +22,36 @@ class Solution {
         }
 
         int[] dist = new int[n + 1];
-        int[] visited = new int[n + 1];
+        // int[] visited = new int[n + 1];
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[k] = 0;
 
-        for (int i = 0; i <= n; i++) {
-            int x = minDist(dist, visited);
-            if (x == -1) {
-                break;
+        PriorityQueue<Pair> pq=new PriorityQueue<Pair>((a,b)-> a.weight-b.weight);
+        pq.add(new Pair(0, k));
+
+        while(!pq.isEmpty()){
+            Pair curr=pq.poll();
+            int currNode=curr.node;
+            int currWeight=curr.weight;
+            for (Pair p : adj[currNode]) {
+                if(dist[currNode]+p.weight<dist[p.node]){
+                    dist[p.node]=dist[currNode]+p.weight;
+                    pq.add(new Pair(dist[p.node], p.node));
+                }
             }
-            visited[x] = 1;
-            for (Pair p : adj[x]) {
-                dist[p.node] = Math.min(dist[p.node], dist[x] + p.weight);
-            }
+
         }
+
+        // for (int i = 0; i <= n; i++) {
+        //     int x = minDist(dist, visited);
+        //     if (x == -1) {
+        //         break;
+        //     }
+        //     visited[x] = 1;
+        //     for (Pair p : adj[x]) {
+        //         dist[p.node] = Math.min(dist[p.node], dist[x] + p.weight);
+        //     }
+        // }
 
         int ans = 0;
         for (int i = 1; i <= n; i++) {
@@ -45,15 +61,15 @@ class Solution {
 
     }
 
-    private int minDist(int[] dist, int[] visited) {
-        int minValue = Integer.MAX_VALUE;
-        int minIndex = -1;
-        for (int i = 1; i < dist.length; i++) {
-            if (visited[i] == 0 && dist[i] < minValue) {
-                minValue = dist[i];
-                minIndex = i;
-            }
-        }
-        return minIndex;
-    }
+    // private int minDist(int[] dist, int[] visited) {
+    //     int minValue = Integer.MAX_VALUE;
+    //     int minIndex = -1;
+    //     for (int i = 1; i < dist.length; i++) {
+    //         if (visited[i] == 0 && dist[i] < minValue) {
+    //             minValue = dist[i];
+    //             minIndex = i;
+    //         }
+    //     }
+    //     return minIndex;
+    // }
 }
