8d55efb4828e9ccfd153df62b623fab063f31e77
Sarthak Madaan <sarthakmadaan5@gmail.com>
2025-12-29 23:45:23 +0530

    Time: 46 ms (5.04%) | Memory: 47.6 MB (6.31%) - LeetSync

---
+++ Leetcode/801-is-graph-bipartite/is-graph-bipartite.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int[] redblack = new int[n];// 1 red -1 black
+        for (int i = 0; i < n; i++) {
+            if (redblack[i] == 0 && notRedBlack(i, graph, 1, redblack)) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    private boolean notRedBlack(int source, int[][] graph, int color, int[] redblack) {
+        redblack[source] = color;
+
+        for (int i = 0; i < graph[source].length; i++) {
+            int x = graph[source][i];
+            System.out.println("source " + source + " x " + x);
+            if (redblack[x] == color) {
+                return true;
+            }
+            if (redblack[x] == 0) {
+                if (notRedBlack(x, graph, -color, redblack)) {
+                    return true;
+                }
+            }
+
+        }
+        return false;
+    }
+
+}
