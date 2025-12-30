88e131361842078366ca6aa8b8393311782139f7
Sarthak Madaan <sarthakmadaan5@gmail.com>
2025-12-30 21:34:12 +0530

    Time: 6 ms (66.71%) | Memory: 63.6 MB (27.03%) - LeetSync

--- Leetcode/820-find-eventual-safe-states/find-eventual-safe-states.java
+++ Leetcode/820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,42 +1,35 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
+        List<Integer> list=new ArrayList<>();
 
-        Set<Integer> s = new HashSet<>();
-        for (int i = 0; i < graph.length; i++) {
-            if (graph[i].length == 0) {
-                s.add(i);
-            }
-        }
         int[] state = new int[graph.length];
         for (int i = 0; i < graph.length; i++) {
-            if (state[i] == 0) {
-                dfs(i, graph, s, state);
+            if(state[i]==2){
+                list.add(i);
+            }
+            if (state[i] == 0 && dfs(i, graph, state)) {
+                list.add(i);
             }
         }
-        List<Integer> list = new ArrayList<>(s);
         Collections.sort(list);
         return list;
 
     }
 
-    public boolean dfs(int source, int[][] adj, Set<Integer> tnSet, int[] state) {
+    public boolean dfs(int source, int[][] adj, int[] state) {
         if (state[source] == 1) {
             return false;
         }
         if (state[source] == 2) {
             return true;
         }
-        if (tnSet.contains(source)) {
-            return true;
-        }
         state[source] = 1;
         for (int a : adj[source]) {
-            if (!dfs(a, adj, tnSet, state)) {
+            if (!dfs(a, adj, state)) {
                 return false;
             }
         }
         state[source] = 2;
-        tnSet.add(source);
         return true;
     }
 }
