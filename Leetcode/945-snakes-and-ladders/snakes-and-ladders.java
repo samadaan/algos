2dff86dd6df6a6d3cf8a46c56ce025527e17891d
Sarthak Madaan <sarthakmadaan5@gmail.com>
2026-01-01 20:16:56 +0530

    Time: 12 ms (17.22%) | Memory: 46.8 MB (40.98%) - LeetSync

--- Leetcode/945-snakes-and-ladders/snakes-and-ladders.java
+++ Leetcode/945-snakes-and-ladders/snakes-and-ladders.java
@@ -1,13 +1,6 @@
 class Solution {
     public int snakesAndLadders(int[][] board) {
         int n = board.length;
-        // for (int i = 0; i < n; i++) {
-        //     for (int j = 0; j < n; j++) {
-        //         System.out.print(board[i][j] + " ");
-        //     }
-        //     System.out.println();
-        // }
-        // return -1;
         Queue<Integer> queue = new LinkedList<>();
         Set<Integer> set = new HashSet<>();
         queue.add(1);
@@ -23,11 +16,9 @@ class Solution {
                 if (num == n * n) {
                     return count;
                 }
-                for (int i = num + 1; i <=Math.min(num + 6, n * n); i++) {
+                for (int i = num + 1; i <= Math.min(num + 6, n * n); i++) {
                     int xCord = findX(i, n);
                     int yCord = findY(i, n);
-                    // System.out
-                    //         .println("i " + i + " xCord " + xCord + " yCord " + yCord + " val " + board[xCord][yCord]);
                     if (board[xCord][yCord] != -1) {
                         queue.add(board[xCord][yCord]);
                     } else {
@@ -41,25 +32,23 @@ class Solution {
     }
 
     private int findX(int num, int n) {
-        int fromEnd = num / n;
+        int div = num / n;
         if (num % n == 0)
-            fromEnd--;
-        return n - fromEnd - 1;
+            div--;
+        return n - div - 1;
     }
 
     private int findY(int num, int n) {
         int div = num / n;
-
-        int rem = num % n;
-        if(rem==0)div--;
-        if (div % 2 == 0) {
-            if (rem == 0) {
-                return n - 1;
-            }
-            return rem - 1;
-        }
+        int rem= num%n;
         if (rem == 0) {
+            div--;
+            if (div % 2 == 0)
+                return n - 1;
             return rem;
+        }
+        if (div % 2 == 0) {
+            return rem - 1;
         }
         return n - rem;
     }
