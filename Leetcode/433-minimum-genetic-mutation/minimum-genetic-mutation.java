19b5b5c1a4fa757e94f90efd309adf1aa7b0bc66
Sarthak Madaan <sarthakmadaan5@gmail.com>
2026-01-01 16:56:52 +0530

    Time: 1 ms (92.20%) | Memory: 42.9 MB (29.18%) - LeetSync

--- Leetcode/433-minimum-genetic-mutation/minimum-genetic-mutation.java
+++ Leetcode/433-minimum-genetic-mutation/minimum-genetic-mutation.java
@@ -4,28 +4,24 @@ class Solution {
         Queue<String> queue = new LinkedList<>();
         Set<String> visited = new HashSet<>();
         int ans = 0;
-        boolean found = false;
         queue.add(startGene);
+        visited.add(startGene);
         while (!queue.isEmpty()) {
             int size = queue.size();
             while (size-- > 0) {
                 String s = queue.poll();
                 if (s.equals(endGene)) {
-                    found = true;
-                    queue.clear();
-                    break;
+                    return ans;
                 }
-                visited.add(s);
                 for (String x : bank) {
                     if (!visited.contains(x) && charDiff(s, x) == 1) {
                         queue.add(x);
+                        visited.add(x);
                     }
                 }
             }
             ans++;
         }
-        if (found)
-            return ans-1;
         return -1;
 
     }
