f02f260c2fa06e427cc905e346afa92674ad4aff
Sarthak Madaan <sarthakmadaan5@gmail.com>
2026-01-02 21:34:13 +0530

    Time: 19 ms (98.86%) | Memory: 47.8 MB (73.15%) - LeetSync

--- Leetcode/127-word-ladder/word-ladder.java
+++ Leetcode/127-word-ladder/word-ladder.java
@@ -1,18 +1,16 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> wordSet = new HashSet<>(wordList);
-        if (!wordSet.contains(endWord)) return 0;
-
         Set<String> beginSet = new HashSet<>();
         Set<String> endSet = new HashSet<>();
-        
         beginSet.add(beginWord);
         endSet.add(endWord);
-        
-        int level = 1;
 
+        Set<String> wordSet = new HashSet<>(wordList);
+        if (!wordSet.contains(endWord)) {
+            return 0;
+        }
+        int levels = 1;
         while (!beginSet.isEmpty() && !endSet.isEmpty()) {
-            // Always expand the smaller set to optimize performance
             if (beginSet.size() > endSet.size()) {
                 Set<String> temp = beginSet;
                 beginSet = endSet;
@@ -21,31 +19,29 @@ class Solution {
 
             Set<String> nextSet = new HashSet<>();
             for (String word : beginSet) {
-                char[] chars = word.toCharArray();
-                
-                for (int i = 0; i < chars.length; i++) {
-                    char old = chars[i];
+                char[] arr = word.toCharArray();
+
+                for (int i = 0; i < arr.length; i++) {
+                    char old = arr[i];
                     for (char c = 'a'; c <= 'z'; c++) {
-                        chars[i] = c;
-                        String target = String.valueOf(chars);
+                        arr[i] = c;
+                        String x = String.valueOf(arr);
 
-                        // If the frontiers meet, we found the shortest path
-                        if (endSet.contains(target)) {
-                            return level + 1;
+                        if (endSet.contains(x)) {
+                            return levels + 1;
                         }
-
-                        if (wordSet.contains(target)) {
-                            nextSet.add(target);
-                            wordSet.remove(target); // Mark as visited
+                        if (wordSet.contains(x)) {
+                            nextSet.add(x);
+                            wordSet.remove(x);
                         }
                     }
-                    chars[i] = old;
+                    arr[i] = old;
                 }
             }
             beginSet = nextSet;
-            level++;
+            levels++;
         }
-        
         return 0;
+
     }
 }
