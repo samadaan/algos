ef9fed981b218eb152b96555f7696b9c1db13c04
Sarthak Madaan <sarthakmadaan5@gmail.com>
2026-01-10 23:34:50 +0530

    Time: 14 ms (75.67%) | Memory: 47.9 MB (19.82%) - LeetSync

--- Leetcode/1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ Leetcode/1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -6,30 +6,25 @@ class Solution {
         int m = grid[0].length;
         int count = 1;
         Queue<int[]> queue = new LinkedList<>();
-        if(grid[0][0]==1)return -1;
+        if (grid[0][0] == 1)
+            return -1;
         queue.offer(new int[] { 0, 0 });
         while (!queue.isEmpty()) {
             int size = queue.size();
             while (size-- > 0) {
                 int[] a = queue.poll();
-                // System.out.println(a[0]+"  "+a[1]);
                 if (a[0] == n - 1 && a[1] == m - 1) {
                     return count;
                 }
                 for (int[] dir : dirs) {
                     int nx = a[0] + dir[0];
                     int ny = a[1] + dir[1];
-                    
-                    // System.out.println("adding "+nx+" "+ny);
-                    
                     if (isValid(nx, ny, n, m) && grid[nx][ny] == 0) {
-                        // System.out.println("added "+nx+" "+ny);
                         grid[nx][ny] = 1;
                         queue.add(new int[] { nx, ny });
                     }
                 }
             }
-            // System.out.println();
             count++;
         }
         return -1;
