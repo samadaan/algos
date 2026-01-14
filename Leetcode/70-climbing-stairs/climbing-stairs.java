04af592f1780269e8c1cd482d091ea58e6496ae4
Sarthak Madaan <sarthakmadaan5@gmail.com>
2026-01-14 19:45:12 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (8.15%) - LeetSync

--- Leetcode/70-climbing-stairs/climbing-stairs.java
+++ Leetcode/70-climbing-stairs/climbing-stairs.java
@@ -1,34 +1,15 @@
 class Solution {
+
     public int climbStairs(int n) {
-        if(n==0)return 0;
-        if(n==1)return 1;
-        int prev0=1;
-        int prev1=1;
-        int temp=0;
+        int prev=1;
+        int curr=1;
+        int temp;
         for(int i=2;i<=n;i++){
-            temp=prev0+prev1;
-            prev0=prev1;
-            prev1=temp;
+            temp=curr;
+            curr=prev+curr;
+            prev=temp;
+            
         }
-        return prev1;
-
+        return curr;
     }
-    // recurisve solution
-    //     if(n==0)return 1;
-    //     if(n<0)return 0;
-    //     return climbStairs(n-1)+climbStairs(n-2);
-
-    // memoization
-    // int[] dp=new int[n+1];
-    //     for(int i=0;i<dp.length;i++){
-    //         dp[i]=-1;
-    //     }
-    //     return climbStairs(dp, n);
-    // public int climbStairs(int[] dp, int n){
-    //     if(n==0)return 1;
-    //     if(n<0)return 0;
-    //     if(dp[n]!=-1)return dp[n];
-    //     return dp[n]=climbStairs(dp, n-1)+climbStairs(dp, n-2);
-    // }
-
 }
