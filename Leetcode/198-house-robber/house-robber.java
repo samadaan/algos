2f1d8783047b612193b4ffb9ab61a60a14442f37
Sarthak Madaan <sarthakmadaan5@gmail.com>
2026-01-15 10:52:19 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (79.04%) - LeetSync

--- Leetcode/198-house-robber/house-robber.java
+++ Leetcode/198-house-robber/house-robber.java
@@ -2,12 +2,35 @@ class Solution {
     int[][] dp;
 
     public int rob(int[] nums) {
-        dp = new int[nums.length][2];
-        for (int i = 0; i < nums.length; i++) {
-            dp[i][0] = -1;
-            dp[i][1] = -1;
+        // dp = new int[nums.length][2];
+        // for (int i = 0; i < nums.length; i++) {
+        //     dp[i][0] = -1;
+        //     dp[i][1] = -1;
+        // }
+        // return rec(nums, 0, 1);
+        int n=nums.length-1;
+        // dp[n][0]=0;
+        // dp[n][1]=nums[n];
+        // for(int i=n-2;i>=0;i--){
+        //     dp[i][0]=dp[i+1][1];
+        //     dp[i][1]=Math.max(nums[i]+dp[i+1][0], dp[i+1][1]);
+        // }
+        // return dp[0][1];
+
+        // int[][] ndp=new int[2][2];
+        // ndp[1][0]=0;
+        // ndp[1][1]=nums[n];
+        int canChoose=nums[n];
+        int canNotChoose=0;
+
+        for(int i=n-1;i>=0;i--){
+            int canNotChoose1=canChoose;
+            int canChoose1=Math.max(nums[i]+canNotChoose, canChoose);
+            // System.out.println("i "+i+" canNotChoose1 "+ canNotChoose1+" canChoose1 "+canChoose1);
+            canChoose=canChoose1;
+            canNotChoose=canNotChoose1;
         }
-        return rec(nums, 0, 1);
+        return canChoose;
 
     }
 
