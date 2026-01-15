2085a6333a28df84d32e278b16eb1fc7db1437fa
Sarthak Madaan <sarthakmadaan5@gmail.com>
2026-01-15 22:51:43 +0530

    Time: 25 ms (32.63%) | Memory: 46.7 MB (42.16%) - LeetSync

--- Leetcode/322-coin-change/coin-change.java
+++ Leetcode/322-coin-change/coin-change.java
@@ -1,55 +1,28 @@
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        // int[] dp = new int[amount+1];
-        // Arrays.fill(dp, -1);
-        // int val = recurse(amount, coins);
-        // int val=memorize(dp, amount, coins);
-        // return val == Integer.MAX_VALUE?-1:val;
-         int val=tabulate(amount, coins);
-        return val == Integer.MAX_VALUE?-1:val;
-    }
-    // public int recurse(int remAmount, int[] coins){
-    //     if(remAmount==0)return 0;
-    //     int min=Integer.MAX_VALUE;
-    //     for(int c:coins){
-    //         if(remAmount>=c){
-    //             min=Math.min(min, recurse(remAmount-c, coins));
-    //             if(min!=Integer.MAX_VALUE)min++;
-    //         }
-    //     }
-    //     return min;
-    // }
+    int[] c;
+    long[] memo;
 
-    // public int memorize(int[] cache, int remAmount, int[] coins){
-    //     if(remAmount==0)return 0;
-    //     if(cache[remAmount]!=-1){
-    //         //System.out.println("remAmount "+remAmount +" cache "+cache[remAmount]);
-    //         return cache[remAmount];
-    //     }
-    //     int min=Integer.MAX_VALUE;
-    //     for(int c:coins){
-    //         if(remAmount>=c){
-    //             int val =  memorize(cache, remAmount-c, coins);
-    //             if(val!=Integer.MAX_VALUE)val++;
-    //             min=Math.min(min, val);
-    //         }
-    //     }
-    //     return cache[remAmount]=min;
-    // }
-    public int tabulate(int amount, int[] coins){
-        int[] dp=new int[amount+1];
-        Arrays.fill(dp,Integer.MAX_VALUE);
-        dp[0]=0;
-        for(int i=1;i<=amount;i++){
-            for(int j=0;j<coins.length;j++){
-                int currAmount=coins[j];
-                if(i>=currAmount){
-                    int newVal=dp[i-currAmount];
-                    if(newVal!=Integer.MAX_VALUE)newVal++;
-                    dp[i]=Math.min(dp[i], newVal);
-                }
-            }
+    public int coinChange(int[] coins, int amount) {
+        c = coins;
+        memo=new long[amount+1];
+        int val = (int) rec(amount);
+        if (val == Integer.MAX_VALUE)
+            return -1;
+        return val;
+
+    }
+
+    public long rec(int remAmount) {
+        if (remAmount < 0)
+            return Integer.MAX_VALUE;
+        if (remAmount == 0)
+            return 0;
+        if(memo[remAmount]!=0)return memo[remAmount];
+        long min = Integer.MAX_VALUE;
+        for (int i = 0; i < c.length; i++) {
+            min = Math.min(min, rec(remAmount - c[i]));
         }
-        return dp[amount];
+        memo[remAmount] = min == Integer.MAX_VALUE ? min : 1 + min;
+        return memo[remAmount];
     }
 }
