bc9fecf9c318d7d5d79576b9b60b04d84ae2af72
Sarthak Madaan <sarthakmadaan5@gmail.com>
2026-01-15 22:59:19 +0530

    Time: 18 ms (44.08%) | Memory: 46.3 MB (64.16%) - LeetSync

--- Leetcode/322-coin-change/coin-change.java
+++ Leetcode/322-coin-change/coin-change.java
@@ -3,12 +3,24 @@ class Solution {
     long[] memo;
 
     public int coinChange(int[] coins, int amount) {
-        c = coins;
-        memo=new long[amount+1];
-        int val = (int) rec(amount);
-        if (val == Integer.MAX_VALUE)
-            return -1;
-        return val;
+        // c = coins;
+        // memo=new long[amount+1];
+        // int val = (int) rec(amount);
+        // if (val == Integer.MAX_VALUE)
+        //     return -1;
+        // return val;
+
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int am = 1; am <= amount; am++) {
+            for (int i = 0; i < coins.length; i++) {
+                if (am - coins[i] >= 0 && dp[am - coins[i]] != Integer.MAX_VALUE) {
+                    dp[am] = Math.min(dp[am], 1 + dp[am - coins[i]]);
+                }
+            }
+        }
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
 
     }
 
@@ -17,7 +29,8 @@ class Solution {
             return Integer.MAX_VALUE;
         if (remAmount == 0)
             return 0;
-        if(memo[remAmount]!=0)return memo[remAmount];
+        if (memo[remAmount] != 0)
+            return memo[remAmount];
         long min = Integer.MAX_VALUE;
         for (int i = 0; i < c.length; i++) {
             min = Math.min(min, rec(remAmount - c[i]));
