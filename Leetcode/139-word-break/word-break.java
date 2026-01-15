18212cbafad7a4ad9f4b7cf0dfa07412512831a6
Sarthak Madaan <sarthakmadaan5@gmail.com>
2026-01-15 22:39:18 +0530

    Time: 9 ms (24.38%) | Memory: 46.3 MB (23.06%) - LeetSync

--- Leetcode/139-word-break/word-break.java
+++ Leetcode/139-word-break/word-break.java
@@ -1,25 +1,19 @@
 class Solution {
-    Boolean[] memo;
-    Set<String> set;
     public boolean wordBreak(String s, List<String> wordDict) {
-        set=new HashSet<>(wordDict);
-        memo=new Boolean[s.length()];
-        return rec(s, 0);
 
-        
-    }
-    public boolean rec(String s, int start){
-        // System.out.println(s+" "+start);
-        if(start==s.length())return true;
+        Set<String> words=new HashSet<>(wordDict);
 
-        if(memo[start]!=null)return memo[start];
-
-        for(int i=start+1; i<=s.length();i++){
-            // System.out.println(i);
-            if(set.contains(s.substring(start, i)) && rec(s, i)){
-                return memo[start]=true;
+        boolean[] dp=new boolean[s.length()+1];
+        dp[0]=true;
+        for(int len=1;len<=s.length();len++){
+            for(int i=0;i<len;i++){
+                if(dp[i] && words.contains(s.substring(i, len))){
+                    dp[len]=true;
+                    break;
+                }
             }
         }
-        return memo[start]=false;
+        return dp[s.length()];
+        
     }
 }
