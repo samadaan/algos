b0119739da0104b9bcb32499fcb17fafae2d9e6a
Sarthak Madaan <sarthakmadaan5@gmail.com>
2026-01-15 23:02:33 +0530

    Time: 16 ms (65.87%) | Memory: 46.5 MB (53.74%) - LeetSync

--- Leetcode/322-coin-change/coin-change.java
+++ Leetcode/322-coin-change/coin-change.java
@@ -1,41 +1,20 @@
 class Solution {
-    int[] c;
-    long[] memo;
+    // int[] c;
+    // long[] memo;
 
     public int coinChange(int[] coins, int amount) {
-        // c = coins;
-        // memo=new long[amount+1];
-        // int val = (int) rec(amount);
-        // if (val == Integer.MAX_VALUE)
-        //     return -1;
-        // return val;
-
+        int max = amount + 1; // Faster than Integer.MAX_VALUE to avoid overflow logic
         int[] dp = new int[amount + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
+        Arrays.fill(dp, max);
         dp[0] = 0;
-        for (int am = 1; am <= amount; am++) {
-            for (int i = 0; i < coins.length; i++) {
-                if (am - coins[i] >= 0 && dp[am - coins[i]] != Integer.MAX_VALUE) {
-                    dp[am] = Math.min(dp[am], 1 + dp[am - coins[i]]);
+
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
             }
         }
-        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
-
-    }
-
-    public long rec(int remAmount) {
-        if (remAmount < 0)
-            return Integer.MAX_VALUE;
-        if (remAmount == 0)
-            return 0;
-        if (memo[remAmount] != 0)
-            return memo[remAmount];
-        long min = Integer.MAX_VALUE;
-        for (int i = 0; i < c.length; i++) {
-            min = Math.min(min, rec(remAmount - c[i]));
-        }
-        memo[remAmount] = min == Integer.MAX_VALUE ? min : 1 + min;
-        return memo[remAmount];
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
