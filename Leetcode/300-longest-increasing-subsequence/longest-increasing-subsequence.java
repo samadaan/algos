4a4f61bed9dcf5f82b916a508fc71ce1f5f901ac
Sarthak Madaan <sarthakmadaan5@gmail.com>
2026-01-17 12:26:14 +0530

    Time: 437 ms (5.06%) | Memory: 273.1 MB (6.32%) - LeetSync

--- Leetcode/300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ Leetcode/300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,18 +1,27 @@
 class Solution {
+    int[] n;
+    int len;
+    Integer[][] memo;
+
     public int lengthOfLIS(int[] nums) {
-        int[] dp=new int[nums.length];
-        Arrays.fill(dp, 1);
-        int ans=1;
-        for(int i=1;i<nums.length;i++){
-            for(int j=0;j<i;j++){
-                if(nums[i]>nums[j]){
-                    if(1+dp[j]>dp[i]){
-                        dp[i]=1+dp[j];
-                        ans=Math.max(ans, dp[i]);
-                    }
-                }
-            }
+        n = nums;
+        len = nums.length;
+        memo = new Integer[nums.length][nums.length + 1];
+        return recur(0, -1);
+
+    }
+
+    public int recur(int index, int prevIndex) {
+        if (index >= len) {
+            return 0;
         }
-        return ans;
+        if (memo[index][prevIndex + 1] != null) {
+            return memo[index][prevIndex + 1];
+        }
+        int len = recur(index + 1, prevIndex);
+        if (prevIndex == -1 || n[index] > n[prevIndex]) {
+            len = Math.max(len, 1 + recur(index + 1, index));
+        }
+        return memo[index][prevIndex + 1] = len;
     }
 }
