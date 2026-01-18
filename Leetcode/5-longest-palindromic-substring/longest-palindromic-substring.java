6a8a12ecee027d7f8342d0f6a2a353f3c0ddafaf
Sarthak Madaan <sarthakmadaan5@gmail.com>
2026-01-18 18:37:13 +0530

    Time: 113 ms (30.52%) | Memory: 48.5 MB (7.76%) - LeetSync

--- Leetcode/5-longest-palindromic-substring/longest-palindromic-substring.java
+++ Leetcode/5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,76 +1,28 @@
 class Solution {
-    String ans;
-    int maxLen;
-    int[][] memo;
-
     public String longestPalindrome(String s) {
-        // maxLen = 0;
-        // ans = "";
-        // memo = new int[s.length()][s.length()];
-        // for (int i = 0; i < s.length(); i++) {
-        //     for (int j = 0; j < s.length(); j++) {
-        //         memo[i][j] = -1;
-        //     }
-        // }
-        // recur(0, s.length() - 1, s);
-        // return ans;
-        if(s.isEmpty())return "";
-        int maxLen=0;
-        String ans=s.substring(0,1);
-        int[][] dp=new int[s.length()][s.length()];
-        for(int i=0;i<s.length();i++){
-            dp[i][i]=1;
-        }
-        for(int len=2;len<=s.length();len++){
-            for(int i=0;i<=s.length()-len;i++){
-                int j=i+len-1;
-                if(s.charAt(i)==s.charAt(j) && isPalindrome(s, i, j)){
-                    if(j-i+1>maxLen){
-                        maxLen=j-i+1;
-                        ans=s.substring(i, j+1);
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int start = 0, maxLen = 1;
+
+        // Length 1
+        for (int i = 0; i < n; i++)
+            dp[i][i] = true;
+
+        // Length 2 and more
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (len == 2 || dp[i + 1][j - 1]) {
+                        dp[i][j] = true;
+                        if (len > maxLen) {
+                            start = i;
+                            maxLen = len;
+                        }
                     }
-                    dp[i][j]=j-i+1;
-                }else{
-                    dp[i][j]=Math.max(dp[i+1][j], dp[i][j-1]);
                 }
             }
         }
-        return ans;
-    }
-    public int recur(int startIndex, int endIndex, String s) {
-        if (startIndex >= s.length() || endIndex >= s.length() || startIndex < 0 || endIndex < 0)
-            return 0;
-        if (startIndex == endIndex){
-            if(maxLen==0){
-                maxLen=1;
-                ans=s.substring(startIndex, endIndex+1);
-            }
-            return memo[startIndex][endIndex]=1;
-        }
-        if (memo[startIndex][endIndex] != -1)
-            return memo[startIndex][endIndex];
-        if (s.charAt(startIndex) == s.charAt(endIndex) && isPalindrome(s, startIndex + 1, endIndex - 1)) {
-            if (endIndex - startIndex + 1 > maxLen) {
-                maxLen = endIndex - startIndex + 1;
-                ans = s.substring(startIndex, endIndex + 1);
-            }
-            return memo[startIndex][endIndex] = endIndex - startIndex + 1;
-        }
-        return memo[startIndex][endIndex] = Math.max(recur(startIndex + 1, endIndex, s),
-                recur(startIndex, endIndex - 1, s));
-
-    }
-
-    public boolean isPalindrome(String s, int startIndex, int endIndex) {
-        // System.out.println("startIndex "+startIndex+" endIndex "+endIndex);
-        while (endIndex > startIndex) {
-            if (s.charAt(startIndex) != s.charAt(endIndex)) {
-                return false;
-            }
-            startIndex++;
-            endIndex--;
-        }
-        // System.out.println("startIndex "+startIndex+" endIndex "+endIndex + " palindrome ");
-        return true;
+        return s.substring(start, start + maxLen);
     }
 }
