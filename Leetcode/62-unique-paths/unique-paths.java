9e54210d3d4c89178ca6b0e0c138feb2b263e9bd
Sarthak Madaan <sarthakmadaan5@gmail.com>
2026-02-08 15:21:54 +0530

    Time: 1 ms (14.95%) | Memory: 41.9 MB (95.78%) - LeetSync

--- Leetcode/62-unique-paths/unique-paths.java
+++ Leetcode/62-unique-paths/unique-paths.java
@@ -2,8 +2,21 @@ class Solution {
     Integer[][] memo;
     public int uniquePaths(int m, int n) {
         memo=new Integer[m][n];
-        return recur(0, 0, m, n);
+        // return recur(0, 0, m, n);
+        for(int i=0;i<n;i++){
+            memo[m-1][i]=1;
+        }
+        for(int i=0;i<m;i++){
+            memo[i][n-1]=1;
+        }
+        for(int i=m-2;i>=0;i--){
+            for(int j=n-2;j>=0;j--){
+                memo[i][j]=memo[i+1][j]+memo[i][j+1];
+            }
+        }
+        return memo[0][0];
     }
+    // O(n^2) memoization
     int recur(int i, int j, int m, int n){
         if(i>=m || j>=n)return 0;
         if(i==m-1 && j==n-1)return 1;
