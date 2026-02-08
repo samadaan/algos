d2c6b01e8fa6d381b07098c945e7dd60bd3013c2
Sarthak Madaan <sarthakmadaan5@gmail.com>
2026-02-08 13:39:13 +0530

    Time: 73 ms (12.12%) | Memory: 48.1 MB (38.80%) - LeetSync

--- Leetcode/55-jump-game/jump-game.java
+++ Leetcode/55-jump-game/jump-game.java
@@ -1,9 +1,20 @@
 class Solution {
     Boolean memo[];
     public boolean canJump(int[] nums) {
-        memo=new Boolean[nums.length];
-        return recur(0, nums);
+        boolean[] dp=new boolean[nums.length];
+        dp[nums.length-1]=true;
+        for(int i=nums.length-2;i>=0;i--){
+            int k=nums[i];
+            for(int j=i+1;j<=i+k;j++){
+                if(dp[j]){
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[0];
     }
+    // O(n^2)
     boolean recur(int index, int[] nums){
         if(index>=nums.length)return false;
         if(index==nums.length-1)return true;
